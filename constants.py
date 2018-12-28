0ca9f1f76d5d6b7a2e39f22dcae98adf450455bd
Shikhar Johri <30645315+johri002@users.noreply.github.com>
2018-12-28 23:26:32 +0530

    Updated constants.py (#2)
    * Update constants.py
    
    * Update constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,6 @@
+import pygame
+
+#setting defaults
 BOARDHEIGHT = 7
 BOARDWIDTH = 7
 TILESIZE = 92
@@ -9,9 +12,8 @@ FPS = 20
 ANIMATIONSPEED = 6
 BLANK = None
 
-
-# set of colors
-#            R   G    B
+# Set of colors
+#        R   G    B
 GRAY = (100, 100, 100)
 NAVYBLUE = (60, 60, 100)
 WHITE = (255, 255, 255)
@@ -42,10 +44,9 @@ BUTTONTEXTCOLOR = BLACK
 BUTTONCOLOR = WHITE
 MESSAGECOLOR = WHITE
 
-
+# Window defaults
 XMARGIN = int((WINDOWWIDTH - (TILESIZE * BOARDWIDTH + (BOARDWIDTH - 1))) / 2)
 YMARGIN = int((WINDOWHEIGHT - (TILESIZE * BOARDHEIGHT - (BOARDHEIGHT - 1))) / 2)
-
 
 UP = "up"
 DOWN = "down"
@@ -64,10 +65,12 @@ JRIGHTRIGHT = "jumprightright"
 JRIGHTUP = "jumprightup"
 JRIGHTDOWN = "jumprightdown"
 
+# Importing images
 carImg = pygame.image.load("car_11.png")
 carImg1 = pygame.image.load("car_22.png")
 flagImg = pygame.image.load("blue2.jpg")
 flagImg1 = pygame.image.load("red2.jpg")
 hmImg1 = pygame.image.load("hm2.jpg")
 
-FENCELIMIT = 8  # The maximum numbr of fences a player can use
+# The maximum number of fences a player can use
+FENCELIMIT = 8  
