d8d05bed35111cb395af0b283b138b2844be90ff
Shikhar Johri <30645315+johri002@users.noreply.github.com>
2018-12-30 23:50:24 +0530

    Generalized images on code base (#9)

--- constants.py
+++ constants.py
@@ -66,11 +66,11 @@ JRIGHTUP = "jumprightup"
 JRIGHTDOWN = "jumprightdown"
 
 # Importing images
-carImg = pygame.image.load("car_11.png")
-carImg1 = pygame.image.load("car_22.png")
-flagImg = pygame.image.load("blue2.jpg")
-flagImg1 = pygame.image.load("red2.jpg")
-hmImg1 = pygame.image.load("hm2.jpg")
+carImg = pygame.image.load("img/car_11.png")
+carImg1 = pygame.image.load("img/car_22.png")
+flagImg = pygame.image.load("img/blue2.jpg")
+flagImg1 = pygame.image.load("img/red2.jpg")
+hmImg1 = pygame.image.load("img/hm2.jpg")
 
 # The maximum number of fences a player can use
 FENCELIMIT = 8  
