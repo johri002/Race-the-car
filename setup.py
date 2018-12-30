d1b9f8a2687ec06f8f9f18e229f409c8c754bdc9
Shikhar Johri <shikharjohri123@gmail.com>
2018-12-31 00:08:27 +0530

    Setup file improvements

--- setup.py
+++ setup.py
@@ -7,6 +7,33 @@ os.environ["TK_LIBRARY"] = os.path.join(PYTHON_INSTALL_DIR, "tcl", "tk8.6")
 
 executables = [cx_Freeze.Executable("Race_the_car.py")]
 cx_Freeze.setup(
+<<<<<<< HEAD
+	name="RacetheCar",
+	version = "0.1.0",
+	author = "Saurabh Jain",
+	author_email ="notifications@github.com",
+	url = "https://github.com/NJACKWinterOfCode/Race-the-car",
+	description = "A strategy board game made with python. More like a shorter version of chess, \nwhere one needs to stay focused, anticipate the other player's moves and cover \nall the tracks.",
+	options={
+		"build_exe": {
+			"packages": [
+				"pygame",
+				"sys",
+				"pygame.surfarray",
+				"numpy",
+				"pygame._numpysurfarray",
+			],
+			"include_files": [
+				"img/red2.jpg",
+				"img/blue2.jpg",
+				"img/car_11.png",
+				"img/car_22.png",
+				"img/hm2.jpg",
+			],
+		}
+	},
+	executables=executables,
+=======
     name="RacetheCar",
     options={
         "build_exe": {
@@ -27,4 +54,5 @@ cx_Freeze.setup(
         }
     },
     executables=executables,
+>>>>>>> 78140d14faab53a2fa2d5483c04144e066741d1f
 )
